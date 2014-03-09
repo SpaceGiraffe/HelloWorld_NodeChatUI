28ac7c46a9f9a2e25fc6c41894e9263f4f484293
Sam Wechter <damelgon@gmail.com>
2014-03-09 14:56:08 -0400

    init

--- public/js/code.js
+++ public/js/code.js
@@ -1,7 +1,8 @@
+var socket;
 window.onload = function() {
  
     var messages = [];
-    var socket = io.connect(window.location.hostname);
+    socket = io.connect(window.location.hostname);
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
