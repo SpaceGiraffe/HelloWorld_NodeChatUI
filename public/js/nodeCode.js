ea02e5d2768fe886cb77c03174df68414779488d
Sam Wechter <damelgon@gmail.com>
2014-04-06 14:22:20 -0400

    Trying to fix ui event listeners (take 9)

--- public/js/nodeCode.js
+++ public/js/nodeCode.js
@@ -58,7 +58,6 @@ function setUIEventHandlers() {
 			var uiElementID = "text1";
 			console.log(uiElementID);
 			var uiElementVal = $("#" + uiElementID).val();
-		}
 	} else {
 		$('.ipadUiElement').on('change', function(){
 			var uiElementID = $(this).attr("id");
